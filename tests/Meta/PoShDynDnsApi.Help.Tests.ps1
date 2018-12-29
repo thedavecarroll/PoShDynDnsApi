986b5644d5724515ff4ccf23976a7e489936865b
Dave Carroll <powershell@anovelidea.org>
2018-12-28 23:07:28 -0600

    fix filename for tests

--- tests/Meta/PoShDynDnsApi.Help.Tests.ps1
+++ tests/Meta/PoShDynDnsApi.Help.Tests.ps1
@@ -5,7 +5,7 @@
 # Remove all versions of the module from the session. Pester can't handle multiple versions.
 Get-Module $env:BHProjectName | Remove-Module
 Import-Module $env:BHModulePath -Verbose:$false -ErrorAction Stop
-$moduleVersion = (Test-ModuleManifest $env:BHPSModuleManifest | select -ExpandProperty Version).ToString()
+$moduleVersion = (Test-ModuleManifest $env:BHPSModuleManifest | Select-Object -ExpandProperty Version).ToString()
 $ms = [Microsoft.PowerShell.Commands.ModuleSpecification]@{ ModuleName = $env:BHProjectName; RequiredVersion = $moduleVersion }
 $commands = Get-Command -FullyQualifiedModule $ms -CommandType Cmdlet, Function, Workflow  # Not alias
 
