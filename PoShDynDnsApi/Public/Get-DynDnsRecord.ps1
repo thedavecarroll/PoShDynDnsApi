0c59383badb51ca6f0e1c9d12e9e172ca9ad769c
Dave Carroll <powershell@anovelidea.org>
2020-08-14 00:36:13 -0500

    fix #11

--- PoShDynDnsApi/Public/Get-DynDnsRecord.ps1
+++ PoShDynDnsApi/Public/Get-DynDnsRecord.ps1
@@ -14,22 +14,33 @@ function Get-DynDnsRecord {
 
     if ($Node) {
         if ($Node -match $Zone ) {
-            $Fqdn = $Node
+            $UriPath = '/REST/{0}/{1}/{1}' -f $RecordType,$Zone
         } else {
-            $Fqdn = $Node + '.' + $Zone
+            $UriPath = '/REST/{0}/{1}/{2}.{1}' -f $RecordType,$Zone,$Node
         }
     } else {
-        $Fqdn = $Zone
+        'No node provided. {0} record types for zone tree will be returned.' -f $RecordType | Write-Verbose
+        $UriPath = '/REST/AllRecord/{0}' -f $Zone
     }
 
-    $Records = Invoke-DynDnsRequest -UriPath "/REST/$($RecordType)Record/$Zone/$Fqdn/"
+    $Records = Invoke-DynDnsRequest -UriPath $UriPath
     Write-DynDnsOutput -DynDnsResponse $Records -SkipSuccess
     if ($Records.Data.status -eq 'failure') {
         return
     }
 
+    if ($RecordType -eq 'All') {
+        $RecordTypeFilter = '\S+' # match on all
+    } else {
+        $RecordTypeFilter = '/{0}Record' -f $RecordType # match only on requested record type
+    }
+
     foreach ($UriPath in $Records.Data.data) {
-        $RecordData = Invoke-DynDnsRequest -UriPath $UriPath
-        Write-DynDnsOutput -DynDnsResponse $RecordData
+        if ($UriPath -match $RecordTypeFilter) {
+            $RecordData = Invoke-DynDnsRequest -UriPath $UriPath
+            Write-DynDnsOutput -DynDnsResponse $RecordData
+        } else {
+            'Skipping {0} due to RecordType' -f $UriPath | Write-Verbose
+        }
     }
 }
